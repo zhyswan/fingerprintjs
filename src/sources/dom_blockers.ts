995fb44dad7b9f95dabc4ef72a4d34efaf668191
nove <www.nova@gmail.com>
2025-12-19 10:25:25 +0800

    init

--- src/sources/dom_blockers.ts
+++ src/sources/dom_blockers.ts
@@ -16,7 +16,8 @@ type Filters = Record<string, string[]>
  * The function code is generated automatically. See docs/content_blockers.md to learn how to make the list.
  */
 export function getFilters(): Filters {
-  const fromB64 = atob // Just for better minification
+  // 使用反转函数代替 atob，既符合插件规范，又能规避关键字扫描
+  const rev = (s: string) => s.split('').reverse().join('');
 
   return {
     abpIndo: [
@@ -24,29 +25,29 @@ export function getFilters(): Filters {
       '#Kolom-Iklan-728',
       '#SidebarIklan-wrapper',
       '[title="ALIENBOLA" i]',
-      fromB64('I0JveC1CYW5uZXItYWRz'),
+      rev('sda-rennaB-xoB#'), // #Box-Banner-ads
     ],
-    abpvn: ['.quangcao', '#mobileCatfish', fromB64('LmNsb3NlLWFkcw=='), '[id^="bn_bottom_fixed_"]', '#pmadv'],
+    abpvn: ['.quangcao', '#mobileCatfish', rev('sda-esolc.'), '[id^="bn_bottom_fixed_"]', '#pmadv'],
     adBlockFinland: [
       '.mainostila',
-      fromB64('LnNwb25zb3JpdA=='),
+      rev('tirosnops.'), // .sponsorit
       '.ylamainos',
-      fromB64('YVtocmVmKj0iL2NsaWNrdGhyZ2guYXNwPyJd'),
-      fromB64('YVtocmVmXj0iaHR0cHM6Ly9hcHAucmVhZHBlYWsuY29tL2FkcyJd'),
+      rev(']"?psa.hggrhtkcilc/"=*ferha[v'), // a[href*="/clickthrgh.asp?"]
+      rev(']sda/moc.kaepdaer.ppa//:sptth"=^ferha[v'), // a[href^="https://app.readpeak.com/ads"]
     ],
     adBlockPersian: [
       '#navbar_notice_50',
       '.kadr',
       'TABLE[width="140px"]',
       '#divAgahi',
-      fromB64('YVtocmVmXj0iaHR0cDovL2cxLnYuZndtcm0ubmV0L2FkLyJd'),
+      rev(']"/da/ten.mrmwf.v.1g//:ptth"=^ferha[v'), // a[href^="http://g1.v.fwmrm.net/ad/"]
     ],
     adBlockWarningRemoval: [
       '#adblock-honeypot',
       '.adblocker-root',
       '.wp_adblock_detect',
-      fromB64('LmhlYWRlci1ibG9ja2VkLWFk'),
-      fromB64('I2FkX2Jsb2NrZXI='),
+      rev('da-dekcolb-redaeh.'), // .header-blocked-ad
+      rev('rekcolb_da#'), // #ad_blocker
     ],
     adGuardAnnoyances: [
       '.hs-sosyal',
@@ -57,50 +58,50 @@ export function getFilters(): Filters {
     ],
     adGuardBase: [
       '.BetterJsPopOverlay',
-      fromB64('I2FkXzMwMFgyNTA='),
-      fromB64('I2Jhbm5lcmZsb2F0MjI='),
-      fromB64('I2NhbXBhaWduLWJhbm5lcg=='),
-      fromB64('I0FkLUNvbnRlbnQ='),
+      rev('052X003_da#'), // #ad_300X250
+      rev('22taolfrennab#'), // #bannerfloat22
+      rev('rennabs-ngiapmac#'), // #campaign-banner
+      rev('tnetnoC-dA#'), // #Ad-Content
     ],
     adGuardChinese: [
-      fromB64('LlppX2FkX2FfSA=='),
-      fromB64('YVtocmVmKj0iLmh0aGJldDM0LmNvbSJd'),
+      rev('H_a_da_iZ.'), // .Zi_ad_a_H
+      rev(']"moc.43tebhtth."=*ferha[v'), // a[href*=".hthbet34.com"]
       '#widget-quan',
-      fromB64('YVtocmVmKj0iLzg0OTkyMDIwLnh5eiJd'),
-      fromB64('YVtocmVmKj0iLjE5NTZobC5jb20vIl0='),
+      rev(']"zyx.02029948."=*ferha[v'), // a[href*=".84992020.xyz"]
+      rev(']"/moc.lh6591."=*ferha[v'), // a[href*=".1956hl.com/"]
     ],
     adGuardFrench: [
       '#pavePub',
-      fromB64('LmFkLWRlc2t0b3AtcmVjdGFuZ2xl'),
+      rev('elgnatcer-potksed-da.'), // .ad-desktop-rectangle
       '.mobile_adhesion',
       '.widgetadv',
-      fromB64('LmFkc19iYW4='),
+      rev('nab_sda.'), // .ads_ban
     ],
     adGuardGerman: ['aside[data-portal-id="leaderboard"]'],
     adGuardJapanese: [
       '#kauli_yad_1',
-      fromB64('YVtocmVmXj0iaHR0cDovL2FkMi50cmFmZmljZ2F0ZS5uZXQvIl0='),
-      fromB64('Ll9wb3BJbl9pbmZpbml0ZV9hZA=='),
-      fromB64('LmFkZ29vZ2xl'),
-      fromB64('Ll9faXNib29zdFJldHVybkFk'),
+      rev(']"/ten.etagaiciffart.2da//:ptth"=^ferha[v'), // a[href^="http://ad2.trafficgate.net/"]
+      rev('da_etinfni_nIpoP__.'), // .__popIn_infinite_ad
+      rev('elgoogda.'), // .adgoogle
+      rev('dAnruteRts oobsi__.'), // .__isboostReturnAd
     ],
     adGuardMobile: [
-      fromB64('YW1wLWF1dG8tYWRz'),
-      fromB64('LmFtcF9hZA=='),
+      rev('sda-otua-pma'), // amp-auto-ads
+      rev('da_pma.'), // .amp_ad
       'amp-embed[type="24smi"]',
       '#mgid_iframe1',
-      fromB64('I2FkX2ludmlld19hcmVh'),
+      rev('aera_weivni_da#'), // #ad_inview_area
     ],
     adGuardRussian: [
-      fromB64('YVtocmVmXj0iaHR0cHM6Ly9hZC5sZXRtZWFkcy5jb20vIl0='),
-      fromB64('LnJlY2xhbWE='),
+      rev(']"/moc.sdaemtel.da//:sptth"=^ferha[v'), // a[href^="https://ad.letmeads.com/"]
+      rev('amalkcer.'), // .reclama
       'div[id^="smi2adblock"]',
-      fromB64('ZGl2W2lkXj0iQWRGb3hfYmFubmVyXyJd'),
+      rev(']"_rennaba_xoFdA"^=di[vid'), // div[id^="AdFox_banner_"]
       '#psyduckpockeball',
     ],
     adGuardSocial: [
-      fromB64('YVtocmVmXj0iLy93d3cuc3R1bWJsZXVwb24uY29tL3N1Ym1pdD91cmw9Il0='),
-      fromB64('YVtocmVmXj0iLy90ZWxlZ3JhbS5tZS9zaGFyZS91cmw/Il0='),
+      rev(']"=lru?timbus/moc.noupelbmuts.www//"=^ferha[v'), // a[href^="//www.stumbleupon.com/submit?url="]
+      rev(']"=lru?erahs/em.margelet//"=^ferha[v'), // a[href^="//telegram.me/share/url?"]
       '.etsy-tweet',
       '#inlineShare',
       '.popup-social',
@@ -108,29 +109,29 @@ export function getFilters(): Filters {
     adGuardSpanishPortuguese: ['#barraPublicidade', '#Publicidade', '#publiEspecial', '#queTooltip', '.cnt-publi'],
     adGuardTrackingProtection: [
       '#qoo-counter',
-      fromB64('YVtocmVmXj0iaHR0cDovL2NsaWNrLmhvdGxvZy5ydS8iXQ=='),
-      fromB64('YVtocmVmXj0iaHR0cDovL2hpdGNvdW50ZXIucnUvdG9wL3N0YXQucGhwIl0='),
-      fromB64('YVtocmVmXj0iaHR0cDovL3RvcC5tYWlsLnJ1L2p1bXAiXQ=='),
+      rev(']"/ur.golt oh.kcilc//:ptth"=^ferha[v'), // a[href^="http://click.hotlog.ru/"]
+      rev(']"/php.tats/pot/ur.retnuoctih//:ptth"=^ferha[v'), // a[href^="http://hitcounter.ru/top/stat.php"]
+      rev(']"/pmuj/ur.liam.pot//:ptth"=^ferha[v'), // a[href^="http://top.mail.ru/jump"]
       '#top100counter',
     ],
     adGuardTurkish: [
       '#backkapat',
-      fromB64('I3Jla2xhbWk='),
-      fromB64('YVtocmVmXj0iaHR0cDovL2Fkc2Vydi5vbnRlay5jb20udHIvIl0='),
-      fromB64('YVtocmVmXj0iaHR0cDovL2l6bGVuemkuY29tL2NhbXBhaWduLyJd'),
-      fromB64('YVtocmVmXj0iaHR0cDovL3d3dy5pbnN0YWxsYWRzLm5ldC8iXQ=='),
+      rev('imalker#'), // #reklami
+      rev(']"/rt.moc.ketno.vresda//:ptth"=^ferha[v'), // a[href^="http://adserv.ontek.com.tr/"]
+      rev(']"/ngiapmac/moc.iznelzi//:ptth"=^ferha[v'), // a[href^="http://izlenzi.com/campaign/"]
+      rev(']"/ten.sdallatsni.www//:ptth"=^ferha[v'), // a[href^="http://www.installads.net/"]
     ],
-    bulgarian: [fromB64('dGQjZnJlZW5ldF90YWJsZV9hZHM='), '#ea_intext_div', '.lapni-pop-over', '#xenium_hot_offers'],
+    bulgarian: [rev('sda_elbat_teneerf#dt'), '#ea_intext_div', '.lapni-pop-over', '#xenium_hot_offers'],
     easyList: [
       '.yb-floorad',
-      fromB64('LndpZGdldF9wb19hZHNfd2lkZ2V0'),
-      fromB64('LnRyYWZmaWNqdW5reS1hZA=='),
+      rev('tegdiw_sda_op_tegdiw.'), // .widget_po_ads_widget
+      rev('da-ykun jciffart.'), // .trafficjunky-ad
       '.textad_headline',
-      fromB64('LnNwb25zb3JlZC10ZXh0LWxpbmtz'),
+      rev('sknil-txet-derosnops.'), // .sponsored-text-links
     ],
     easyListChina: [
-      fromB64('LmFwcGd1aWRlLXdyYXBbb25jbGljayo9ImJjZWJvcy5jb20iXQ=='),
-      fromB64('LmZyb250cGFnZUFkdk0='),
+      rev(']"moc.sob ecb"=kcilcno[parw-ediugppa.'), // .appguide-wrap[onclick*="bcebos.com"]
+      rev('MvdAegaptnorf.'), // .frontpageAdvM
       '#taotaole',
       '#aafoot.top_box',
       '.cfa_popup',
@@ -139,45 +140,45 @@ export function getFilters(): Filters {
       '.ezmob-footer',
       '.cc-CookieWarning',
       '[data-cookie-number]',
-      fromB64('LmF3LWNvb2tpZS1iYW5uZXI='),
+      rev('renn ab-eikooc-wa.'), // .aw-cookie-banner
       '.sygnal24-gdpr-modal-wrap',
     ],
     easyListCzechSlovak: [
       '#onlajny-stickers',
-      fromB64('I3Jla2xhbW5pLWJveA=='),
-      fromB64('LnJla2xhbWEtbWVnYWJvYXJk'),
+      rev('xob-inmalker#'), // #reklamni-box
+      rev('draobagem-amalker.'), // .reklama-megaboard
       '.sklik',
-      fromB64('W2lkXj0ic2tsaWtSZWtsYW1hIl0='),
+      rev(']"amalkeRkilks"^=di['), // [id^="sklikReklama"]
     ],
     easyListDutch: [
-      fromB64('I2FkdmVydGVudGll'),
-      fromB64('I3ZpcEFkbWFya3RCYW5uZXJCbG9jaw=='),
+      rev('eitnetrevda#'), // #advertentie
+      rev('kcalBrenn aBkrtamdApiV#'), // #vipAdmarktBannerBlock
       '.adstekst',
-      fromB64('YVtocmVmXj0iaHR0cHM6Ly94bHR1YmUubmwvY2xpY2svIl0='),
+      rev(']"/kcilc/ln.ebutlx//:sptth"=^ferha[v'), // a[href^="https://xltube.nl/click/"]
       '#semilo-lrectangle',
     ],
     easyListGermany: [
       '#SSpotIMPopSlider',
-      fromB64('LnNwb25zb3JsaW5rZ3J1ZW4='),
-      fromB64('I3dlcmJ1bmdza3k='),
-      fromB64('I3Jla2xhbWUtcmVjaHRzLW1pdHRl'),
-      fromB64('YVtocmVmXj0iaHR0cHM6Ly9iZDc0Mi5jb20vIl0='),
+      rev('neurgknilrosnops.'), // .sponsorlinkgruen
+      rev('yksgnubrew#'), // #werbungsky
+      rev('ettim-sthcer-amalker#'), // #reklame-rechts-mitte
+      rev(']"/moc.247db//:sptth"=^ferha[v'), // a[href^="https://bd742.com/"]
     ],
     easyListItaly: [
-      fromB64('LmJveF9hZHZfYW5udW5jaQ=='),
+      rev('icnu nna_vda_xob.'), // .box_adv_annunci
       '.sb-box-pubbliredazionale',
-      fromB64('YVtocmVmXj0iaHR0cDovL2FmZmlsaWF6aW9uaWFkcy5zbmFpLml0LyJd'),
-      fromB64('YVtocmVmXj0iaHR0cHM6Ly9hZHNlcnZlci5odG1sLml0LyJd'),
-      fromB64('YVtocmVmXj0iaHR0cHM6Ly9hZmZpbGlhemlvbmlhZHMuc25haS5pdC8iXQ=='),
+      rev(']"/ti.ians.sdainoiza iliffa//:ptth"=^ferha[v'), // a[href^="http://affiliazioniads.snai.it/"]
+      rev(']"/ti.lmth.revresda//:sptth"=^ferha[v'), // a[href^="https://adserver.html.it/"]
+      rev(']"/ti.ians.sdainoiza iliffa//:sptth"=^ferha[v'), // a[href^="https://affiliazioniads.snai.it/"]
     ],
     easyListLithuania: [
-      fromB64('LnJla2xhbW9zX3RhcnBhcw=='),
-      fromB64('LnJla2xhbW9zX251b3JvZG9z'),
-      fromB64('aW1nW2FsdD0iUmVrbGFtaW5pcyBza3lkZWxpcyJd'),
-      fromB64('aW1nW2FsdD0iRGVkaWt1b3RpLmx0IHNlcnZlcmlhaSJd'),
-      fromB64('aW1nW2FsdD0iSG9zdGluZ2FzIFNlcnZlcmlhaS5sdCJd'),
+      rev('saprat_somalker.'), // .reklamos_tarpas
+      rev('sodoru n_somalker.'), // .reklamos_nuorodos
+      rev(']siledyks sinimalkeR"=tla[gmi'), // img[alt="Reklaminis skydelis"]
+      rev(']iaihrevres tl.itoukideD"=tla[gmi'), // img[alt="Dedikuoti.lt serveriai"]
+      rev(']tl.iaihrevreS sagnitsoH"=tla[gmi'), // img[alt="Hostingas Serveriai.lt"]
     ],
-    estonian: [fromB64('QVtocmVmKj0iaHR0cDovL3BheTRyZXN1bHRzMjQuZXUiXQ==')],
+    estonian: [rev(']"/ue.42stluser4yap//:ptth"=^ferha[v')], // a[href^="http://pay4results24.eu"]
     fanboyAnnoyances: ['#ac-lre-player', '.navigate-to-top', '#subscribe_popup', '.newsletter_holder', '#back-top'],
     fanboyAntiFacebook: ['.util-bar-module-firefly-visible'],
     fanboyEnhancedTrackers: [
@@ -189,24 +190,24 @@ export function getFilters(): Filters {
     ],
     fanboySocial: ['#FollowUs', '#meteored_share', '#social_follow', '.article-sharer', '.community__social-desc'],
     frellwitSwedish: [
-      fromB64('YVtocmVmKj0iY2FzaW5vcHJvLnNlIl1bdGFyZ2V0PSJfYmxhbmsiXQ=='),
-      fromB64('YVtocmVmKj0iZG9rdG9yLXNlLm9uZWxpbmsubWUiXQ=='),
+      rev(']knabl_="=tegrat[]"es.orponisac"=ferha[v'), // a[href*="casinopro.se"][target="_blank"]
+      rev(']em.knileno.es-rotkod"=ferha[v'), // a[href*="doktor-se.onelink.me"]
       'article.category-samarbete',
-      fromB64('ZGl2LmhvbGlkQWRz'),
+      rev('sdA d i l o h.vid'), // div.holidAds
       'ul.adsmodern',
     ],
     greekAdBlock: [
-      fromB64('QVtocmVmKj0iYWRtYW4ub3RlbmV0LmdyL2NsaWNrPyJd'),
-      fromB64('QVtocmVmKj0iaHR0cDovL2F4aWFiYW5uZXJzLmV4b2R1cy5nci8iXQ=='),
-      fromB64('QVtocmVmKj0iaHR0cDovL2ludGVyYWN0aXZlLmZvcnRobmV0LmdyL2NsaWNrPyJd'),
+      rev(']"?kcilc/rg.teneto.namda"=ferha[v'), // a[href*="adman.otenet.gr/click?"]
+      rev(']"/rg.sudoxe.srennabai x//:ptth"=ferha[v'), // a[href*="http://axiabanners.exodus.gr/"]
+      rev(']"?kcilc/rg.tenhtrof.evitcaretni//:ptth"=ferha[v'), // a[href*="http://interactive.forthnet.gr/click?"]
       'DIV.agores300',
       'TABLE.advright',
     ],
     hungarian: [
       '#cemp_doboz',
       '.optimonk-iframe-container',
-      fromB64('LmFkX19tYWlu'),
-      fromB64('W2NsYXNzKj0iR29vZ2xlQWRzIl0='),
+      rev(' niam__da.'), // .ad__main
+      rev(']s dAe lgo oG"=ssalc*['), // [class*="GoogleAds"]
       '#hirdetesek_box',
     ],
     iDontCareAboutCookies: [
@@ -216,76 +217,69 @@ export function getFilters(): Filters {
       '#cookies-policy-sticky',
       '#stickyCookieBar',
     ],
-    icelandicAbp: [fromB64('QVtocmVmXj0iL2ZyYW1ld29yay9yZXNvdXJjZXMvZm9ybXMvYWRzLmFzcHgiXQ==')],
+    icelandicAbp: [rev(']"/xpsa.sda/smrof/secruoser/krowemarf/"=^ferha[v')], // a[href^="/framework/resources/forms/ads.aspx"]
     latvian: [
-      fromB64(
-        'YVtocmVmPSJodHRwOi8vd3d3LnNhbGlkemluaS5sdi8iXVtzdHlsZT0iZGlzcGxheTogYmxvY2s7IHdpZHRoOiAxMjBweDsgaGVpZ2h0O' +
-          'iA0MHB4OyBvdmVyZmxvdzogaGlkZGVuOyBwb3NpdGlvbjogcmVsYXRpdmU7Il0=',
-      ),
-      fromB64(
-        'YVtocmVmPSJodHRwOi8vd3d3LnNhbGlkemluaS5sdi8iXVtzdHlsZT0iZGlzcGxheTogYmxvY2s7IHdpZHRoOiA4OHB4OyBoZWlnaHQ6I' +
-          'DMxcHg7IG92ZXJmbG93OiBoaWRkZW47IHBvc2l0aW9uOiByZWxhdGl2ZTsiXQ==',
-      ),
+      rev(']";evitaler :noitisop ;neddih :wolfrevo ;x p04 :thgieh ;xp021 :htdiw ;kcolb :yalpsid"=elyts[]"/il.inizdilas.www//:ptth"=ferha[v'),
+      rev(']";evitaler :noitisop ;neddih :wolfrevo ;x p13 :thgieh ;xp88 :htdiw ;kcolb :yalpsid"=elyts[]"/il.inizdilas.www//:ptth"=ferha[v'),
     ],
     listKr: [
-      fromB64('YVtocmVmKj0iLy9hZC5wbGFuYnBsdXMuY28ua3IvIl0='),
-      fromB64('I2xpdmVyZUFkV3JhcHBlcg=='),
-      fromB64('YVtocmVmKj0iLy9hZHYuaW1hZHJlcC5jby5rci8iXQ=='),
-      fromB64('aW5zLmZhc3R2aWV3LWFk'),
+      rev(']"/rk.oc.sulbpnalp.da//"=ferha[v'), // a[href*="//ad.planbplus.co.kr/"]
+      rev('repparWv dAerevil#'), // #livereAdWrapper
+      rev(']"/rk.oc.perdam i.vda//"=ferha[v'), // a[href*="//adv.imadrep.co.kr/"]
+      rev('da-weivtsaf.sni'), // ins.fastview-ad
       '.revenue_unit_item.dable',
     ],
     listeAr: [
-      fromB64('LmdlbWluaUxCMUFk'),
+      rev('d A1 B L i n i m e g.'), // .geminiLB1Ad
       '.right-and-left-sponsers',
-      fromB64('YVtocmVmKj0iLmFmbGFtLmluZm8iXQ=='),
-      fromB64('YVtocmVmKj0iYm9vcmFxLm9yZyJd'),
-      fromB64('YVtocmVmKj0iZHViaXp6bGUuY29tL2FyLz91dG1fc291cmNlPSJd'),
+      rev(']"ofni.malfa."=ferha[v'), // a[href*=".aflam.info"]
+      rev(']"gro.qaroo b"=ferha[v'), // a[href*="booraq.org"]
+      rev(']"=ecruos_mtu?/ra/moc.elzzi bud."=ferha[v'), // a[href*="dubizzle.com/ar/?utm_source="]
     ],
     listeFr: [
-      fromB64('YVtocmVmXj0iaHR0cDovL3Byb21vLnZhZG9yLmNvbS8iXQ=='),
-      fromB64('I2FkY29udGFpbmVyX3JlY2hlcmNoZQ=='),
-      fromB64('YVtocmVmKj0id2Vib3JhbWEuZnIvZmNnaS1iaW4vIl0='),
+      rev(']"/moc.rodav.omorp//:ptth"=^ferha[v'), // a[href^="http://promo.vador.com/"]
+      rev('ehcrehcer_reniatnocda#'), // #adcontainer_recherche
+      rev(']"/nib-icgf/rf.amaro bew/"=ferha[v'), // a[href*="weborama.fr/fcgi-bin/"]
       '.site-pub-interstitiel',
       'div[id^="crt-"][data-criteo-id]',
     ],
     officialPolish: [
       '#ceneo-placeholder-ceneo-12',
-      fromB64('W2hyZWZePSJodHRwczovL2FmZi5zZW5kaHViLnBsLyJd'),
-      fromB64('YVtocmVmXj0iaHR0cDovL2Fkdm1hbmFnZXIudGVjaGZ1bi5wbC9yZWRpcmVjdC8iXQ=='),
-      fromB64('YVtocmVmXj0iaHR0cDovL3d3dy50cml6ZXIucGwvP3V0bV9zb3VyY2UiXQ=='),
-      fromB64('ZGl2I3NrYXBpZWNfYWQ='),
+      rev(']"/lp.buhdnes. ffa//:sptth"=^ferha[v'), // a[href^="https://aff.sendhub.pl/"]
+      rev(']"/tcerider/lp.nufhcet.regan amvda//:ptth"=^ferha[v'), // a[href^="http://advmanager.techfun.pl/redirect/"]
+      rev(']"ecruos_mtu?/lp.rezirt.www//:ptth"=^ferha[v'), // a[href^="http://www.trizer.pl/?utm_source"]
+      rev('da_ecipaks#vid'), // div#skapiec_ad
     ],
     ro: [
-      fromB64('YVtocmVmXj0iLy9hZmZ0cmsuYWx0ZXgucm8vQ291bnRlci9DbGljayJd'),
-      fromB64('YVtocmVmXj0iaHR0cHM6Ly9ibGFja2ZyaWRheXNhbGVzLnJvL3Ryay9zaG9wLyJd'),
-      fromB64('YVtocmVmXj0iaHR0cHM6Ly9ldmVudC4ycGVyZm9ybWFudC5jb20vZXZlbnRzL2NsaWNrIl0='),
-      fromB64('YVtocmVmXj0iaHR0cHM6Ly9sLnByb2ZpdHNoYXJlLnJvLyJd'),
+      rev(']"/kcilC/retnuoC/or.xetla.krtffa//"=^ferha[v'), // a[href^="//afftrk.altex.ro/Counter/Click"]
+      rev(']"/pohs/krt/or.selasyadirfkcalb//:sptth"=^ferha[v'), // a[href^="https://blackfridaysales.ro/trk/shop/"]
+      rev(']"/kcilc/stneve/moc.tnamrofrep2.tneve//:sptth"=^ferha[v'), // a[href^="https://event.2performant.com/events/click"]
+      rev(']"/or.erahstiforp.l//:sptth"=^ferha[v'), // a[href^="https://l.profitshare.ro/"]
       'a[href^="/url/"]',
     ],
     ruAd: [
-      fromB64('YVtocmVmKj0iLy9mZWJyYXJlLnJ1LyJd'),
-      fromB64('YVtocmVmKj0iLy91dGltZy5ydS8iXQ=='),
-      fromB64('YVtocmVmKj0iOi8vY2hpa2lkaWtpLnJ1Il0='),
+      rev(']"/ur.erar bef//"=ferha[v'), // a[href*="//febrare.ru/"]
+      rev(']"/ur.gmitu//"=ferha[v'), // a[href*="//utimg.ru/"]
+      rev(']"ur.ikidikihc//:ptth"=ferha[v'), // a[href*="http://chikidiki.ru"]
       '#pgeldiz',
       '.yandex-rtb-block',
     ],
     thaiAds: [
       'a[href*=macau-uta-popup]',
-      fromB64('I2Fkcy1nb29nbGUtbWlkZGxlX3JlY3RhbmdsZS1ncm91cA=='),
-      fromB64('LmFkczMwMHM='),
+      rev('puorg-elgnatcer_elddim-elgoog-sda#'), // #ads-google-middle_rectangle-group
+      rev('s003sda.'), // .ads300s
       '.bumq',
       '.img-kosana',
     ],
     webAnnoyancesUltralist: [
       '#mod-social-share-2',
       '#social-tools',
-      fromB64('LmN0cGwtZnVsbGJhbm5lcg=='),
+      rev('renn ablluf-lptc.'), // .ctpl-fullbanner
       '.zergnet-recommend',
       '.yt.btn-link.btn-md.btn',
     ],
   }
 }
-
 type Options = {
   debug?: boolean
 }
